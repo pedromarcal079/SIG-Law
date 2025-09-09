59f0ea6124b8a08c0c0257edc27645255da18eb2
Vanesca <vanesca.santos.115@ufrn.edu.br>
2025-09-08 22:45:10 -0300

    style: padronizar mensagem de opção inválida

--- siglaw.c
+++ siglaw.c
@@ -56,7 +56,11 @@ int main(void) {
                     getchar();
                     break;
                 default:
-                    printf("Voce digitou uma opcao invalida\n");
+                    printf("+---------------------------------------------------------------------------------------------+\n");
+                    printf("|                                                                                             |\n");
+                    printf("|                              Você digitou uma opção inválida!                               |\n");
+                    printf("|                                                                                             |\n");
+                    printf("+---------------------------------------------------------------------------------------------+\n");
                     printf("Pressione ENTER ... \n");
                     getchar();
                     break;
