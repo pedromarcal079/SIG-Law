262163aeea3ae7e4e73a3e9521026eba8f799ba0
Vanesca <vanesca.santos.115@ufrn.edu.br>
2025-09-08 22:21:28 -0300

    refactor: modularizar empresa em empresa.c empresa.h

---
+++ empresa.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "empresa.h"
+
+int menuEmpresa(void) {
+    system("clear");
+    int opcaoEmpresa;
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                      Modulo de Empresa                                      |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                          1 - Cadastra empresa                                               |\n");
+    printf("|                          2 - Mostra empresa                                                 |\n");
+    printf("|                          3 - Edita empresa                                                  |\n");
+    printf("|                          4 - Exclui empresa                                                 |\n");
+    printf("|                          0 - Voltar                                                         |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("===> Digite sua opcao: ");
+    scanf("%d",&opcaoEmpresa);
+    getchar();
+    return opcaoEmpresa;
+}
+
+
+void cadastraEmpresa(void) {
+    system("clear");
+    char cnpj[15];
+    char nome[50];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                       Cadastrar Empresa                                     |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|        Informe os dados da empresa:                                                         |\n");
+    printf("|   ===> CNPJ: ");
+    fgets(cnpj, sizeof(cnpj), stdin);
+    printf("|   ===> Nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Empresa cadastrada com sucesso!                                                      |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
+
+
+void mostraEmpresa(void){
+    system("clear");
+    char cnpj[15];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                    Mostrar Empresa                                          |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|   ===> Digite o CNPJ da empresa: ");
+    fgets(cnpj, sizeof(cnpj), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Nome: Empresa Legal                                                                  |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
+
+
+void editaEmpresa(void) {
+    system("clear");
+    char cnpj[15];
+    char nome[50];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                     Editar Empresa                                          |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|   ===> Digite o CNPJ da empresa: ");
+    fgets(cnpj, sizeof(cnpj), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Digite os novos dados:                                                               |\n");
+    printf("|        Nome: ");
+    fgets(nome, sizeof(nome), stdin);  
+    printf("|                                                                                             |\n");
+    printf("|        Dados atualizados com sucesso!                                                       |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
+
+
+void excluiEmpresa(void) {
+    system("clear");
+    char cnpj[15];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                    Excluir Empresa                                          |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|   ===> Digite o CNPJ da empresa: ");
+    fgets(cnpj, sizeof(cnpj), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Empresa excluida com sucesso!                                                        |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
---
+++ empresa.h
@@ -0,0 +1,10 @@
+#ifndef EMPRESA_H
+#define EMPRESA_H
+
+int menuEmpresa(void);
+void cadastraEmpresa(void);
+void mostraEmpresa(void);
+void editaEmpresa(void);
+void excluiEmpresa(void);
+
+#endif
