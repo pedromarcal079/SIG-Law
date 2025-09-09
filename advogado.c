3366c9480238b645c9c16c7ebb7d6045404fe750
Vanesca <vanesca.santos.115@ufrn.edu.br>
2025-09-08 22:04:41 -0300

    refactor: modularizar advogado em advogado.c e advogado.h

---
+++ advogado.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "advogado.h"
+
+int menuAdvogado(void) {
+    system("clear");
+    int opcao;
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                     Modulo de Advogados                                     |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                               1 - Cadastra Advogado                                         |\n");
+    printf("|                               2 - Mostra Advogado                                           |\n");
+    printf("|                               3 - Edita Advogado                                            |\n");
+    printf("|                               4 - Exclui Processo                                           |\n");
+    printf("|                               0 - Sair                                                      |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("===> Digite sua opcao: ");
+    scanf("%d",&opcao);
+    getchar();
+    return opcao;
+}
+
+
+void cadastraAdvogado(void) {
+    system("clear");
+    char cpf[15];
+    char nome[50];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                    Cadastrar Advogado                                       |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|        Informe os dados do advogado:                                                        |\n");
+    printf("|   ===> CPF: ");
+    fgets(cpf, sizeof(cpf), stdin);
+    printf("|   ===> Nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Advogado cadastrado com sucesso!                                                     |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
+
+
+void mostraAdvogado(void){
+    system("clear");
+    char cpf[15];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                    Mostrar Advogado                                         |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|   ===> Digite o cpf do advogado: ");
+    fgets(cpf, sizeof(cpf), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Nome: Advogado Fulano de Town                                                        |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
+
+
+void editaAdvogado(void) {
+    system("clear");
+    char cpf[15];
+    char nome[50];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                     Editar Advogado                                         |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|   ===> Digite o cpf do advogado: ");
+    fgets(cpf, sizeof(cpf), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Digite os novos dados:                                                               |\n");
+    printf("|        Nome: ");
+    fgets(nome, sizeof(nome), stdin);  
+    printf("|                                                                                             |\n");
+    printf("|        Dados atualizados com sucesso!                                                       |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
+
+
+void excluiAdvogado(void) {
+    system("clear");
+    char cpf[15];
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                    Excluir Advogado                                         |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|   ===> Digite o cpf do advogado: ");
+    fgets(cpf, sizeof(cpf), stdin);
+    printf("|                                                                                             |\n");
+    printf("|        Advogado excluido com sucesso!                                                       |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+}
