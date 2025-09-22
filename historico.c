d00f5a3b1aa66df58ab047adfb4bbc942566e856
Pedro William <pedromarcal079@ufrn.edu.br>
2025-09-22 15:15:56 -0300

    feat: Criação módulo histórico

---
+++ historico.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "historico.h"
+
+void moduloHistorico(void){
+
+}
+
+int menuHistorico(void) {
+    return 0;
+}
+
+void cadastraHistorico(void) {
+
+} 
+
+void mostraHistorico(void){
+
+}
+
+void editaHistorico(void){
+
+}
+
+void excluiHistorico(void){
+
+}
---
+++ historico.h
@@ -0,0 +1,11 @@
+#ifndef EMPRESA_H
+#define EMPRESA_H
+
+void moduloHistorico(void);
+int menuHistorico(void);
+void cadastraHistorico(void);
+void mostraHistorico(void);
+void editaHistorico(void);
+void excluiHistorico(void);
+
+#endif
