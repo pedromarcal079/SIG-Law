575bcb80358a5f9e96d3df66221cc70779e7e94f
Pedro William <pedromarcal079@ufrn.edu.br>
2025-09-22 15:23:22 -0300

    feat: cria função cadastramento de histórico

--- historico.c
+++ historico.c
@@ -7,7 +7,25 @@ void moduloHistorico(void){
 }
 
 int menuHistorico(void) {
-    return 0;
+    system("clear");
+    int opcaoHistorico;
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                                      Modulo de Histórico                                    |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("|                                                                                             |\n");
+    printf("|                          1 - Cadastra histórico                                             |\n");
+    printf("|                          2 - Mostra histórico                                               |\n");
+    printf("|                          3 - Edita histórico                                                |\n");
+    printf("|                          4 - Exclui histórico                                               |\n");
+    printf("|                          0 - Voltar                                                         |\n");
+    printf("|                                                                                             |\n");
+    printf("+---------------------------------------------------------------------------------------------+\n");
+    printf("===> Digite sua opcao: ");
+    scanf("%d",&opcaoHistorico);
+    getchar();
+    return opcaoHistorico;
 }
 
 void cadastraHistorico(void) {
