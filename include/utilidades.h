5284340ff4957d992d442aeb7d48fac185732d1a
Leonardo Relva <leonardo.relva.111@ufrn.edu.br>
2025-11-07 23:49:32 +0000

    Implementacao de utilidades.h e utilidades.c

---
+++ include/utilidades.h
@@ -0,0 +1,3 @@
+void input(char *, int, char *);
+#define True 1;
+#define False 0;
---
+++ src/utilidades.c
@@ -0,0 +1,16 @@
+#include <stdin.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utilidades.h"
+
+void input(char *variavel, int tam, char *mensagem){
+
+    int len;
+    printf("%s", mensagem);
+    fgets(variavel, tam, stdin);
+    printf("\n");
+    len = strlen(variavel);
+    if(len > 0 && variavel[len-1] == '\n'){
+        variavel[len-1] = '\0';
+    }
+}
